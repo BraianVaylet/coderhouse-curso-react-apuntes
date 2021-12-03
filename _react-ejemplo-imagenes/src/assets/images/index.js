8b86c56ed415de4b5511a361d9e6cc9ae0b19426
Braian D. Vaylet <31072177+BraianVaylet@users.noreply.github.com>
2021-12-03 11:29:03 -0300

    Update index.js

--- _react-ejemplo-imagenes/src/assets/images/index.js
+++ _react-ejemplo-imagenes/src/assets/images/index.js
@@ -1,14 +1,3 @@
-// Para mostrar una imagen el React js, tienes dos formas sencillas de hacerlo.
-// 1. Importar la imagen
-// 2. Utilizar la url de una imagen que esta en un servidor remoto o local.
-
-// Ventajas de hacer esto:
-// 1. Tenes mas control sobre las imagenes
-// 2. Podes guardarlas como en este caso en un objeto y nutrirlas de mas propiedades, en este caso le agregamos un nombre
-// que luego usaremos para mostrarlo en el atributo alt de la etiqueta img.
-// 3. Es mas simple de depurar, sabes donde estan todas las imagenes si en el futuro tenes que cambiar una.
-// 4. etc... Todas las ventajas que se te ocurran
-
 import imagew1 from './w1.jpg'
 import imagew2 from './w2.jpg'
 import imagew3 from './w3.jpg'
